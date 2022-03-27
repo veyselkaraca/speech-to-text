29397f8346ed043d46b3348a9d29efd2278012ac
Veysel Karaca <57874373+veyselkaraca@users.noreply.github.com>
2022-03-27 23:42:32 +0300

    :S

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'spech-to-text',
+  appName: 'speech-to-text',
   webDir: 'www',
   bundledWebRuntime: false
 };
